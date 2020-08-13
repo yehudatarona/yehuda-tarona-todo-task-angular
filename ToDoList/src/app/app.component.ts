3c45f1386950de0005c6b0be1fa6dd3915e52cd5
Yehuda Tarona <yehuda.tarona@gmail.com>
2020-08-13 23:51:31 +0300

    13 08 2020

--- ToDoList/src/app/app.component.ts
+++ ToDoList/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Todo } from "./todo"
 import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { apiService } from './ApiService'
 import { from } from 'rxjs';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,21 +11,32 @@ import { from } from 'rxjs';
 })
 export class AppComponent {
   title = 'ToDoList';
+  id: number;
   todovalue: string;
   catvalue: string;
   datevalue: number;
   list: Todo[];
-  // list:[];
+
+  inputDark: string;
+  catDark: string;
+  soryValue:string;
+
   // url: string = "http://localhost:3000/todo";
   url: string = "https://tiba-todo-angular.herokuapp.com/todo";
 
   constructor(private _apiService: apiService) { }
 
   ngOnInit() {
+    console.log(_.isEmpty({})); // returns true
+    this.id = Date.now();
     this.list = [];
     this.todovalue = "";
     this.catvalue = "";
+    this.inputDark = ""
+    this.catDark = "";
+    this.soryValue ="";
     this.datevalue = Date.now();
+
     if (localStorage.getItem("list")) {
       this.list = JSON.parse(localStorage.getItem("list"));
 
@@ -79,6 +91,7 @@ export class AppComponent {
 
 
   deleteItem(id) {
+
     this._apiService.postData(this.url + "/del", { del: id })
       .subscribe(data => {
         this._apiService.getData(this.url)
@@ -108,54 +121,62 @@ export class AppComponent {
 
   // }
 
+  edit(id, name, cat) {
+
+    // alert(id+" "+name+" "+cat)
+    this.id = id;
+    this.inputDark = name
+    this.catDark = cat;
+    document.getElementById("id_dark").className = "dark container-fluid center";
+    this.editItem();
+
+  }
+
   editItem() {
 
-    alert("not completed yet")
+    const newobj: Todo = {
+      id: this.id,
+      name: this.inputDark ,
+      category: this.catDark
+    };
+    
+    this._apiService.postData(this.url+"/edit", newobj )
+      .subscribe(data => {
+        this._apiService.getData(this.url)
+          .subscribe(data => {
+            console.log(data)
+            this.list = data
+          })
 
-    if (this.todovalue !== "" && this.catvalue !== "") {
-      const newItem: Todo = {
-        id: Date.now(),
-        name: this.todovalue,
-        category: this.catvalue
-      };
-
-      this._apiService.postData(this.url + "/edit", newItem)
-        .subscribe(data => {
-          this._apiService.getData(this.url)
-            .subscribe(data => {
-              console.log(data)
-              this.list = data
-            })
-
-        })
-
-
-    }
-    else if (this.todovalue == "" || this.catvalue == "") {
-      alert(" All Fields must be filled");
-
-    }
-
-    console.log(this.list);
-
-    this.todovalue = "";
-    this.catvalue = "";
-    this.datevalue = Date.now();
+      })
+      
 
   }
 
 
   clearAllList() {
-    this._apiService.postData(this.url+"/clearAll", {})
-      .subscribe(data=>{
+    this._apiService.postData(this.url + "/clearAll", {})
+      .subscribe(data => {
         this._apiService.getData(this.url)
-        .subscribe(data => {
-          console.log(data)
-          this.list = data
-        })
+          .subscribe(data => {
+            console.log(data)
+            this.list = data
+          })
       })
-     
-    }
+
+  }
+
+
+  closeDark() {
+    // alert("closeDark");
+    document.getElementById("id_dark").className = "d-none"
+  }
+
+  sortBy(){
+    // alert(this.soryValue);
+    this.list =_.sortBy(this.list,[this.soryValue])
+    this.soryValue="";
+  }
 
   //   clearAllList() {
   //     alert("clearAllList")
@@ -165,6 +186,8 @@ export class AppComponent {
   //   }
 
   // }
+
+
 }
 
 
